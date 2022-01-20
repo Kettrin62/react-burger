e2fa460c59b4cb7a55b99b521bb24fd50eec4050
Kettrin62 <Kettykazakova@yandex.ru>
2022-01-20 12:53:59 +0300

    feat: API

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,7 +4,38 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
+const url= 'https://norma.nomoreparties.space/api/ingredients';
+
 function App() {
+
+  const [state, setState] = React.useState({
+    isLoading: false,
+    hasError: false,
+    data: []
+  });
+
+  React.useEffect(() => {
+    const getIngredients = () => {
+      setState({ ...state, hasError: false, isLoading: true });
+      fetch(url)
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Ошибка: ${res.status}`);
+        })
+        .then(data => setState({ ...state, data: data.data, isLoading: false }))
+        .catch(e => {
+          setState({ ...state, hasError: true, isLoading: false });
+        });
+    };
+    
+    getIngredients();
+  }, []);
+
+  const { data, isLoading, hasError } = state;
+
+
   return (
     <div className='app'>
       <AppHeader />
@@ -12,6 +43,18 @@ function App() {
         <BurgerIngredients />
         <BurgerConstructor />
       </main>
+      <ul>
+        {isLoading && 'Загрузка...'}
+        {hasError && 'Произошла ошибка'}
+        {!isLoading &&
+          !hasError &&
+          data.length &&
+          data.map((item, index) => (
+            <li key={index}>
+              {item.name} {item.price}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
