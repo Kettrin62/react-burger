782b7af4d25f241a8dcf3952161da189455e4304
Kettrin62 <Kettykazakova@yandex.ru>
2022-02-18 09:58:52 +0300

    feat: totalPrice

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
@@ -8,7 +8,6 @@ import burgerconstructorStyles from './burger-constructor.module.css';
 import Modal from '../modal/modal';
 import OrderDetails from '../order-details/order-details';
 import { cardPropTypes } from '../../utils/data';
-import { TotalPriceContext } from '../../services/burger-constructor-context';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOrder } from '../../services/actions/order';
 import { useDrop } from "react-dnd";
@@ -53,18 +52,6 @@ const ConstructorList = () => {
   const { cards, cardBun } = useSelector(state => state.cards);
   const dispatch = useDispatch();
 
-  
-  // const { totalDispatcher } = React.useContext(TotalPriceContext);
-  
-  // const dataConstructor = ingredients.filter(item =>
-  //   cards.find(element => element.id === item._id)
-  //   );
-
-  // const dataConstructorBun = ingredients.filter(item =>
-  //   cards.find(element => element.id === item._id)
-  //   );
-
-  
   const ingredientsBun = ingredients.filter(item => item._id === cardBun);
   
   const ingredientsNotBun = cards.map((item) => {
@@ -81,13 +68,6 @@ const ConstructorList = () => {
     )
   });
 
-  // const ingredientsBun = dataConstructorBun.filter(item => item.type === "bun");
-  // const ingredientsNotBun = dataConstructor.filter(item => item.type !== "bun");
-  
-  // React.useEffect(() => {
-  //   totalDispatcher({ arrayBun: ingredientsBun, arrayNotBun: ingredientsNotBun })
-  // }, [cards, ingredients]);
-  
   const [{isHover}, dropTarget] = useDrop({
     accept: 'ingredient',
     drop(item) {
@@ -110,8 +90,6 @@ const ConstructorList = () => {
   });
   
   const border = isHover ? '1px solid #2f2f37' : 'transparent';
-  
-
 
   return (
     <ul className={'pl-4 pr-4 ' + burgerconstructorStyles.constructorlist} ref={dropTarget} style={{border}}>
@@ -152,8 +130,23 @@ const ConstructorList = () => {
 };
 
 const Total = () => {
-  const { cards } = useSelector(state => state.cards);
-  const { totalPrice } = React.useContext(TotalPriceContext);
+  const { cards, cardBun } = useSelector(state => state.cards);
+  const { ingredients } = useSelector(state => state.ingredients);
+
+  const totalPrice = useMemo(() => {
+    let total = 0;
+      const element = ingredients.find(el => el._id === cardBun);
+      if (element) {
+        total += element.price * 2;
+      };
+    cards.forEach((item) => {
+      const element = ingredients.find(el => el._id === item.id);
+      if (element) {
+        total += element.price;
+      }
+    });
+    return total;
+  }, [cards, ingredients, cardBun]);
 
   const [visible, setVisible] = React.useState(false);
   
@@ -176,7 +169,7 @@ const Total = () => {
   return (
     <div style={{overflow: 'hidden'}} className={'pl-4 pr-6 mt-10 ' + burgerconstructorStyles.total}>
       <p className='text text_type_digits-medium mr-2'>
-        {totalPrice.price}
+        {totalPrice}
       </p>
       <CurrencyIcon type="primary" />
       <div className='ml-10'>
@@ -189,24 +182,12 @@ const Total = () => {
   )
 };
 
-const totalInitialPrice = { price: 0 };
-
-function reducer(_totalPrice, action) {
-  const total = 
-    action.arrayNotBun.reduce((acc, item) => acc + item.price, 0) + 
-    action.arrayBun.reduce((acc, item) => acc + item.price * 2, 0);
-  return { price: total };
-}
-
 function BurgerConstructor() {
-  const [totalPrice, totalDispatcher] = React.useReducer(reducer, totalInitialPrice);
 
   return (
     <section className={'pl-5 pr-5 pt-25 ' + burgerconstructorStyles.section}>
-      <TotalPriceContext.Provider value={{ totalPrice, totalDispatcher }}>
-        <ConstructorList />
-        <Total />
-      </TotalPriceContext.Provider>
+      <ConstructorList />
+      <Total />
     </section>
   )
 }
